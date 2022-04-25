a9a8aa13c39a645f3bdd4612c9142143c2ad7ee9
Hugo Poschel <hugo.poschel@gmail.com>
2022-04-25 16:04:02 +0200

    Modification du fichier : 'Script_Installation_GLPI.py' > Extraction des données du fichier de configuration externe 'config.ini' + définition des variables

--- ScriptInstallationGLPI/Script_Installation_GLPI.py
+++ ScriptInstallationGLPI/Script_Installation_GLPI.py
@@ -17,3 +17,25 @@ import wget
 print("Saisir le chemin d'accès au répertoire dans lequel a été copié le répertoire contenant le script d'installation de GLPI ainsi que les autres fichiers de configuration externes")
 cheminAccesRepertoireContenantRepertoireScriptInstallationGLPI = input()
 cheminAccesFichierDeConfigurationExterneIni = cheminAccesRepertoireContenantRepertoireScriptInstallationGLPI + "/ScriptInstallationGLPI/config.ini"
+
+
+
+fichierDeConfigurationExterneIni = configparser.ConfigParser()
+fichierDeConfigurationExterneIni.read(cheminAccesFichierDeConfigurationExterneIni)
+
+
+nomHoteMariaDB = fichierDeConfigurationExterneIni['Variables']['nomHoteMariaDB']
+nomUtilisateurRootMariaDB = fichierDeConfigurationExterneIni['Variables']['nomUtilisateurRootMariaDB']
+motDePasseUtilisateurRootMariaDB = fichierDeConfigurationExterneIni['Mots De Passe']['motDePasseUtilisateurRootMariaDB']
+nomBaseDeDonneesGLPIMariaDB = fichierDeConfigurationExterneIni['Variables']['nomBaseDeDonneesGLPIMariaDB']
+nomUtilisateurGLPIMariaDB = fichierDeConfigurationExterneIni['Variables']['nomUtilisateurGLPIMariaDB']
+motDePasseUtilisateurGLPIMariaDB = fichierDeConfigurationExterneIni['Mots De Passe']['motDePasseUtilisateurGLPIMariaDB']
+cheminAccesRepertoireDonneesSystemeFuseauxHoraires = fichierDeConfigurationExterneIni['Variables']['cheminAccesRepertoireDonneesSystemeFuseauxHoraires']
+urlListeVersionsGLPI = fichierDeConfigurationExterneIni['Variables']['urlListeVersionsGLPI']
+cheminAccesRepertoireDestinationTelechargementVersionGLPIAInstaller = fichierDeConfigurationExterneIni['Variables']['cheminAccesRepertoireDestinationTelechargementVersionGLPIAInstaller']
+cheminAccesRepertoireInstallationGLPIServeurWebApache = fichierDeConfigurationExterneIni['Variables']['cheminAccesRepertoireInstallationGLPIServeurWebApache']
+cheminAccesRepertoireConfigurationGLPIServeurWebApache = fichierDeConfigurationExterneIni['Variables']['cheminAccesRepertoireConfigurationGLPIServeurWebApache']
+langueInstallationGLPI = fichierDeConfigurationExterneIni['Variables']['langueInstallationGLPI']
+nomVirtualHostGLPIServeurWebApache = fichierDeConfigurationExterneIni['Variables']['nomVirtualHostGLPIServeurWebApache']
+cheminAccesModeleFichierConfigurationVirtualHostGLPIServeurWebApache = fichierDeConfigurationExterneIni['Variables']['cheminAccesModeleFichierConfigurationVirtualHostGLPIServeurWebApache']
+cheminAccesFichierConfigurationVirtualHostGLPIServeurWebApache = fichierDeConfigurationExterneIni['Variables']['cheminAccesFichierConfigurationVirtualHostGLPIServeurWebApache']
